8fda0b86f5b0b661c3fe6fd855d77846de72fe21
Russell Dias <rus321@gmail.com>
2011-11-26 13:32:50 +1000

    floating point fail

--- receive-location.php
+++ receive-location.php
@@ -1,28 +1,20 @@
 <?php
 include 'main.php';
-
 $postdata = file_get_contents("php://input");
 
-$user = json_decode($postdata);
-
+$user = json_decode($postdata, true);
+$user['location'] = array(-27.4673492,153.0246638);
 //$user['location'][0] = lat
 //$user['location'][1] = long
 
-function haversine ($l1, $o1, $l2, $o2) 
-{ 
-    $l1 = deg2rad ($l1); 
-    $sinl1 = sin ($l1); 
-    $l2 = deg2rad ($l2); 
-    $o1 = deg2rad ($o1); 
-    $o2 = deg2rad ($o2); 
-                 
-    return (7926 - 26 * $sinl1) * asin (min (1, 0.707106781186548 * sqrt ((1 - (sin ($l2) * $sinl1) - cos ($l1) * cos ($l2) * cos ($o2 - $o1))))); 
-} 
+//echo getDistanceBetweenPointsNew(-27.5624422072, 153.080918317, -27.4253119313, 153.017472824);
+//die;
 
-$toilets = array_map(function($member) {
-	$member['distance'] = haversine($user['location'][0], $user['location'][1], $member['latitude'], $member['longitude']);
+$toilets = array_map(function($member) use ($user) {
+	$member['distance'] = 1000 * getDistanceBetweenPointsNew($user['location'][0], $user['location'][1], $member['latitude'], $member['longitude']);
 	return $member;
 }, $toilets);
+
 
 usort($toilets, function($a, $b) {
 	return $a['distance'] - $b['distance'];
